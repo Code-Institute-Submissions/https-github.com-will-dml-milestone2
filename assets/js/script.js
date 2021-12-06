d5c39fc02f2f8d3aa945ee7c046f1dbca9932a02
will-dml <will_ou@outlook.fr>
2021-12-06 16:45:44 +0000

    Create initial variables in js

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+/**
+ * Create variables for every elements needed.
+ */
+
+let article = document.getElementsByClassName('wiki-article');
+let choiceA = document.getElementById('A');
+let choiceB = document.getElementById('B');
+let choiceC = document.getElementById('C');
+let choiceD = document.getElementById('D');
+let submitButton = document.getElementsByClassName('submit-button');
+let counter = document.getElementById('counter');
+let returnAnswer = document.getElementById('return-answer');
+
+let questions = [
+    {}
+]
